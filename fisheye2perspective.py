02846cb9857ab55abcb97b0f7a1fecdd21afb1c4
Yuri <Yurri@zju.edu.cn>
2022-05-28 17:07:33 +0800

    use other K

--- fisheye2perspective.py
+++ fisheye2perspective.py
@@ -53,7 +53,7 @@ def readVariable(fid,name,M,N):
     return mat
 
 def K_change(K):
-    times = 0.3
+    times = 0.5
     K_out = K.copy()
     K_out[0,0] *= times
     K_out[1,1] *= times
@@ -72,16 +72,15 @@ if __name__=="__main__":
             k1, k2, p1, p2 = np.array(list(load_distortion(root)))
             gamma1, gamma2, u0, v0 = np.array(list(load_intrinsics(root)))
             K = np.array([[gamma1, 0, u0], [0, gamma2, v0], [0, 0, 1]])
-            K_out = K_change(K)
-            for i in K_out:
-                for j in i:
-                    print(j, end=" ")
-                print('0.0', end=" ")
-            print('0.0 0.0 0.0 1.0')
-            # camera_select = 'P_rect_' + root[-2:]
-            # lastrow = np.array([0,0,0,1]).reshape(1,4)
-            # K = np.concatenate((readVariable(fid, camera_select, 3, 4), lastrow))
-            # K = K_change(K)
+            # K_out = K_change(K)
+            # for i in K_out:
+            #     for j in i:
+            #         print(j, end=" ")
+            #     print('0.0', end=" ")
+            # print('0.0 0.0 0.0 1.0')
+            camera_select = 'P_rect_' + root[-2:]
+            lastrow = np.array([0,0,0,1]).reshape(1,4)
+            K_out = np.concatenate((readVariable(fid, camera_select, 3, 4), lastrow))
             for image_file in tqdm(os.listdir(fisheye_path)):
                 image_path = fisheye_path + '/' + image_file
                 image_path_o = 'data_2d_raw/' + scene + '/' + root + '/data_rgb/' + image_file
@@ -89,7 +88,7 @@ if __name__=="__main__":
                 # plt.imshow(image)
                 # pylab.show()
                 dis_coeff = np.array([k1, k2, p1, p2])
-                out = cv2.omnidir.undistortImage(image, K, dis_coeff, xi, cv2.omnidir.RECTIFY_PERSPECTIVE, Knew=K_out, new_size=(500,500))
+                out = cv2.omnidir.undistortImage(image, K, dis_coeff, xi, cv2.omnidir.RECTIFY_PERSPECTIVE, Knew=K_out[:3,:4], new_size=(1408,376))
                 # out_plt = cv2.cvtColor(out, cv2.COLOR_BGR2RGB)
                 # plt.imshow(out_plt)
                 # pylab.show()
